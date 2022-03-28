c173193b14ed3f6acb403692a16a9f3e315aea44
MAMinhaz <unknownabedin@gmail.com>
2022-03-29 00:07:01 +0600

    topic python operator completed

---
+++ Topic one/bitiwise_operator.py
@@ -0,0 +1,11 @@
+a = 10
+b = 8
+
+# print(bin(a))   #binary value a - 1010
+# print(bin(b))   #binary value b - 1000
+
+print(bin(a&b), a&b)    #a&b binary value - 1000 8
+
+print(bin(a|b), a|b)    #a|b binary value - 1010 10
+
+print(bin(a^b), a^b)    #a^b binary value - 0010 2
