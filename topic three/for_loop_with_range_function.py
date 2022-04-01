2d822cfbfab849012fda10a6bacd0489767f1494
MAMinhaz <unknownabedin@gmail.com>
2022-04-01 23:33:56 +0600

    chapter 18 For Loop with Range function done

---
+++ topic three/for_loop_with_range_function.py
@@ -0,0 +1,34 @@
+"""
+range(5)
+    #start = 0
+    #condition = 4
+    #increament = 1
+
+range(1, 5)
+    #start =1
+    #condition = 5
+    #increament = 1
+
+range(1, 5, 2)
+    #start =1
+    #condition = 5
+    #increament = 2
+"""
+"""
+    for n in range(5):
+        print(n)    #printing for loop with condition only
+
+    print("<br>")
+
+    for n in range(1, 5):
+        print(n)    #printing for loop with specific start point and condition
+
+    print("<br>")
+
+    for n in range(1, 7, 2):
+        print(n)    #printing for loop with specific start point, condition and increment value
+
+"""
+
+for n in range(1, 51, 1):
+    print("2 * ", n, " = ", 2*n)
